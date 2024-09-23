66c65bd878171ec0669f7b1d1a19d5a781cc56d3
Ubuntu <ubuntu@ip-172-31-33-40.us-east-2.compute.internal>
2024-09-23 18:45:46 +0000

    rama principla de hoteles

---
+++ runapp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Usa la versión de Node.js que necesites (por ejemplo, 14)
+nvm use 14.21.3
+
+# Ejecuta tu aplicación Node.js
+node /home/ubuntu/ChatBotWaRentando/src/app.js
--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import { createBot, createFlow, MemoryDB, createProvider, addKeyword } from '@bot-whatsapp/bot';
 import { BaileysProvider, handleCtx } from '@bot-whatsapp/provider-baileys';
-
-const flowBienvenida = addKeyword(['Hola', 'ola']).addAnswer('Hola, soy AviBot🤖', { delay: 500 });
+import http from 'http';
 
 const formatPhoneNumber = (number) => {
     return `521${number}@s.whatsapp.net`;
@@ -9,8 +8,9 @@ const formatPhoneNumber = (number) => {
 
 const main = async () => {
     const provider = createProvider(BaileysProvider);
-
-    provider.initHttpServer(3002);
+    
+    const server = http.createServer(provider.http?.app);
+    provider.initHttpServer(3003);
 
     provider.http?.server.post('/send-note', handleCtx(async (bot, req, res) => {
         const { number, message, mediaURL } = req.body;
@@ -49,41 +49,32 @@ const main = async () => {
         }
     }));
 
-    provider.http?.server.post('/send-message', handleCtx(async (bot, req, res) => {
-        const { number, message } = req.body;
-        const contact = formatPhoneNumber(number);
-
-        try {
-            await bot.sendMessage(contact, message, {});
-            console.log('Mensaje enviado:', message);
-
-            // Responder con éxito
-            res.setHeader('Content-Type', 'application/json');
-            res.end(JSON.stringify({
-                status: 200,
-                status_message: 'Ok',
-                texto: 'Mensaje enviado (*≧ω≦)'
-            }));
-
-        } catch (error) {
-            console.error('Error al enviar mensaje:', error);
-
-            res.setHeader('Content-Type', 'application/json');
-            res.statusCode = 500;
-            res.end(JSON.stringify({
-                status: 500,
-                status_message: 'Error',
-                texto: 'Error al enviar el mensaje (｡•́︿•̀｡)',
-                error: error.message
-            }));
-        }
-    }));
-
     await createBot({
         flow: createFlow([flowBienvenida]),
         database: new MemoryDB(),
         provider,
     });
+
+    // Manejar señales de terminación SIGTERM y SIGINT
+    const shutdown = () => {
+        console.log('Recibida señal de terminación. Cerrando servidor...');
+
+        // Cerrar el servidor HTTP
+        server.close(() => {
+            console.log('Servidor HTTP cerrado.');
+            process.exit(0);
+        });
+
+        // O bien, si necesitas forzar el cierre
+        setTimeout(() => {
+            console.error('Forzando el cierre...');
+            process.exit(1);
+        }, 5000); // Espera de 5 segundos antes de forzar el cierre
+    };
+
+    process.on('SIGTERM', shutdown);
+    process.on('SIGINT', shutdown);
+
 };
 
 main();

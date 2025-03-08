a3a300f2fd650e33aa8c6a46f174ed3dbbe2496b
Bocho <ozkargdg@hotmail.com>
2025-03-07 18:25:02 -0600

    Posible solución de numeros null y NaN

--- src/app.js
+++ src/app.js
@@ -2,8 +2,6 @@ import { createBot, createFlow, MemoryDB, createProvider, addKeyword } from '@bo
 import { BaileysProvider, handleCtx } from '@bot-whatsapp/provider-baileys';
 import http from 'http';
 
-const flowBienvenida = addKeyword(['Hola', 'ola']).addAnswer('Hola, soy AviBot🤖', { delay: 500 });
-
 const formatPhoneNumber = (number) => {
     return `521${number}@s.whatsapp.net`;
 };
@@ -18,68 +16,71 @@ const main = async () => {
         const { number, message, mediaURL } = req.body;
         const contact = formatPhoneNumber(number);
 
-	console.log(req.body);
-
-        try {
-            // Enviar mensaje de bienvenida
-            await bot.sendMessage(contact, message, {});
-            console.log('Mensaje enviado:', message);
-
-            // Enviar archivo si existe una URL
-            if (mediaURL) {
-                await bot.sendMessage(contact, '', { media: mediaURL });
-                console.log('Archivo enviado:', mediaURL);
+        if (number != null && !isNaN(Number(number))) {
+            try {
+                // Enviar mensaje de bienvenida
+                await bot.sendMessage(contact, message, {});
+               // console.log('Mensaje enviado:', message);
+    
+                // Enviar archivo si existe una URL
+                if (mediaURL) {
+                    await bot.sendMessage(contact, '', { media: mediaURL });
+                    //console.log('Archivo enviado:', mediaURL);
+                }
+    
+                // Responder con éxito
+                res.setHeader('Content-Type', 'application/json');
+                res.end(JSON.stringify({
+                    status: 200,
+                    status_message: 'Ok',
+                    texto: 'Mensaje enviado y archivo adjuntado (*≧ω≦)'
+                }));
+    
+            } catch (error) {
+                //console.error('Error al enviar mensaje o archivo:', error);
+    
+                res.setHeader('Content-Type', 'application/json');
+                res.statusCode = 500;
+                res.end(JSON.stringify({
+                    status: 500,
+                    status_message: 'Error',
+                    texto: 'Error al enviar el mensaje o archivo (｡•́︿•̀｡)',
+                    error: error.message
+                }));
             }
-
-            // Responder con éxito
-            res.setHeader('Content-Type', 'application/json');
-            res.end(JSON.stringify({
-                status: 200,
-                status_message: 'Ok',
-                texto: 'Mensaje enviado y archivo adjuntado (*≧ω≦)'
-            }));
-
-        } catch (error) {
-            console.error('Error al enviar mensaje o archivo:', error);
-
-            res.setHeader('Content-Type', 'application/json');
-            res.statusCode = 500;
-            res.end(JSON.stringify({
-                status: 500,
-                status_message: 'Error',
-                texto: 'Error al enviar el mensaje o archivo (｡•́︿•̀｡)',
-                error: error.message
-            }));
         }
     }));
 
     provider.http?.server.post('/send-message', handleCtx(async (bot, req, res) => {
         const { number, message } = req.body;
-        const contact = formatPhoneNumber(number);
 
-        try {
-            await bot.sendMessage(contact, message, {});
-            console.log('Mensaje enviado:', message);
+        if (number != null && !isNaN(Number(number))) {
+            const contact = formatPhoneNumber(number);
 
-            // Responder con éxito
-            res.setHeader('Content-Type', 'application/json');
-            res.end(JSON.stringify({
-                status: 200,
-                status_message: 'Ok',
-                texto: 'Mensaje enviado (*≧ω≦)'
-            }));
-
-        } catch (error) {
-            console.error('Error al enviar mensaje:', error);
-
-            res.setHeader('Content-Type', 'application/json');
-            res.statusCode = 500;
-            res.end(JSON.stringify({
-                status: 500,
-                status_message: 'Error',
-                texto: 'Error al enviar el mensaje (｡•́︿•̀｡)',
-                error: error.message
-            }));
+            try {
+                await bot.sendMessage(contact, message, {});
+                //console.log('Mensaje enviado:', message);
+    
+                // Responder con éxito
+                res.setHeader('Content-Type', 'application/json');
+                res.end(JSON.stringify({
+                    status: 200,
+                    status_message: 'Ok',
+                    texto: 'Mensaje enviado (*≧ω≦)'
+                }));
+    
+            } catch (error) {
+                //console.error('Error al enviar mensaje:', error);
+    
+                res.setHeader('Content-Type', 'application/json');
+                res.statusCode = 500;
+                res.end(JSON.stringify({
+                    status: 500,
+                    status_message: 'Error',
+                    texto: 'Error al enviar el mensaje (｡•́︿•̀｡)',
+                    error: error.message
+                }));
+            }
         }
     }));
 
